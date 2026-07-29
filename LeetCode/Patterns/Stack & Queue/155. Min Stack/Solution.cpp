43b16beb4d6c151309559f74a863a9f1916713a0
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-07-29 19:45:11 +0530

    Add C++ Solution to Patterns/Stack & Queue

---
+++ LeetCode/Patterns/Stack & Queue/155. Min Stack/Solution.cpp
@@ -0,0 +1,46 @@
+class MinStack {
+    stack<int>st;
+    stack<int>st1;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        st.push(value);
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        int mini = INT_MAX;
+        while(!st.empty()){
+            int i = st.top();
+            st.pop();
+            mini = min(mini,i);
+            st1.push(i);
+        }
+        while(!st1.empty()){
+            int i = st1.top();
+            st1.pop();
+            st.push(i);
+        }
+        if(mini == INT_MAX) return -1;
+        return mini;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
