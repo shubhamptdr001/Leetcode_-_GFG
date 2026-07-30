783a40514ac8a2928b29914972f8cbbd2918fc74
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-07-30 16:20:44 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/513. Find Bottom Left Tree Value/Solution.cpp
+++ LeetCode/C++/Medium/513. Find Bottom Left Tree Value/Solution.cpp
@@ -12,34 +12,26 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        map<int,map<int,vector<int>>>m;
+        map<int, vector<int>> level;
 
-        queue<pair<TreeNode*,pair<int,int>>>q;
-        q.push({root,{0,0}});
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
 
         while(!q.empty()){
             int n = q.size();
-            vector<
             while(n--){
                 TreeNode* node = q.front().first;
-                int row = q.front().second.first;
-                int col = q.front().second.second;
-                m[row][col].push_back(node->val);
+                int row = q.front().second;
+                level[row].push_back(node->val);
                 q.pop();
 
-                if(node->left)q.push({node->left,{row+1,col-1}});
-                if(node->right)q.push({node->right,{row+1,col+1}});
+                if(node->left)q.push({node->left,row+1});
+                if(node->right)q.push({node->right,row+1});
             }
         }
         int ans = root->val;
-        for(auto &row:m){
-            vector<int>l;
-            for(auto &col:row.second){
-                for(auto i:col.second){
-                    l.push_back(i);
-                }
-            }
-            ans = l[0];
+        for(auto &row:level){
+           ans = row.second[0];
         }
         return ans;
     }
