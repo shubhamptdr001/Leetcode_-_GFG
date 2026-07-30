1720359af8fbc12fb02cea7ba34a6ad910f90876
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-07-30 13:09:50 +0530

    Add C++ Solution to Patterns/Trees

---
+++ LeetCode/Patterns/Trees/1029. Vertical Order Traversal of a Binary Tree/Solution.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root)return ans;
+
+         map<int, map<int, multiset<int>>> mp;
+        queue<pair<TreeNode*, pair<int,int>>> q;
+
+        q.push({root, {0, 0}});
+
+        while (!q.empty()) {
+
+            auto curr = q.front();
+            q.pop();
+
+            TreeNode* node = curr.first;
+            int col = curr.second.first;
+            int row = curr.second.second;
+
+            // Store node value
+            mp[col][row].insert(node->val);
+
+            if (node->left)
+                q.push({node->left, {col - 1, row + 1}});
+
+            if (node->right)
+                q.push({node->right, {col + 1, row + 1}});
+        }
+        for (auto &col : mp) {
+            vector<int> temp;
+            for (auto &row : col.second) {
+                for (int val : row.second) {
+                    temp.push_back(val);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }    
+};
