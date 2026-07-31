22e0fd3c89d4d399c351c844c833e39ef8584117
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-07-31 09:58:10 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/3014. Minimum Number of Pushes to Type Word I/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+
+        if(n<=8) return n;
+        else if(n<=16) return 8+(n-8)*2;
+        else if(n<=24) return 24+(n-16)*3;
+        return 48+(n-24)*4;
+    }
+};
