cd87cdb954c912bec1a17f52b2df9d9ebe5dadab
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-08-04 14:26:32 +0530

    Time: 8 ms (15.02%), Space: 32 MB (68.12%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*>st;
+    BSTIterator(TreeNode* root) {
+        pushALL(root);
+    }
+    
+    int next() {
+        TreeNode* temp = st.top();
+        st.pop();
+        pushALL(temp->right);
+        return temp->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+    void pushALL(TreeNode* root){
+        for(;root != NULL;st.push(root),root = root->left);
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
