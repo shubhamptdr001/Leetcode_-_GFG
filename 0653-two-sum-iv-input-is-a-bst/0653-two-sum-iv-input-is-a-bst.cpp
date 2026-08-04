cc60349e66247c3885a63e19c034ca6bc82b6c55
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-08-04 15:24:21 +0530

    Time: 0 ms (100%), Space: 37.3 MB (64.19%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+    void inorder(TreeNode* root,vector<int>&v){
+        if(!root)return;
+
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        vector<int>v;
+
+        inorder(root,v);
+        int left=0,right = v.size()-1;
+
+        while(left < right){
+            int sum = v[left] + v[right];
+
+            if(sum == k) return true;
+
+            if(sum < k){
+                left++;
+            }else right --;
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
