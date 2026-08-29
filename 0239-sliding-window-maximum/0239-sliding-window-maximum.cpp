14c73e2e42210225992e2148fe173b75654f09e7
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-08-29 17:18:40 +0530

    Time: 25 ms (48.43%), Space: 139.3 MB (50.21%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int>dq;
+        vector<int>ans;
+
+        for(int i=0;i<n;i++){
+            if(!dq.empty() && dq.front() == i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[dq.back()] <= nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k-1){
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
