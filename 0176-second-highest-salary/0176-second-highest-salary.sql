c26f9751bdd58204cd6970310a98046cebe39a80
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-09-03 12:05:43 +0530

    Time: 3280 ms (5.01%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(1)

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,7 @@
+select max(salary) as SecondHighestSalary
+from employee
+where salary not in (select max(salary) from employee);
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
