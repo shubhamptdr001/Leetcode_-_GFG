935fd948d851f32e1fb9593eeaf9d427601e72d7
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-09-03 11:58:39 +0530

    Time: 1284 ms (6.37%), Space: 0B (100%) - LeetHub - Time: O(N^2), Space: O(1)

---
+++ 0176-delete-duplicate-emails/0176-delete-duplicate-emails.sql
@@ -0,0 +1,8 @@
+delete p1
+from person p1,person p2
+where p1.email = p2.email
+and p1.id > p2.id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
