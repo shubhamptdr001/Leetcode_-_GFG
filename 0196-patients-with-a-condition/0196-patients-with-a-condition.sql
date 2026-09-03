2f9653d99b90b1b8964d8250e66eca6a8f6be19b
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-09-03 11:47:26 +0530

    Time: 487 ms (30.72%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 0196-patients-with-a-condition/0196-patients-with-a-condition.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select *
+from patients
+where conditions like 'DIAB1%'
+or conditions like '% DIAB1%'
+order by patient_id;
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
