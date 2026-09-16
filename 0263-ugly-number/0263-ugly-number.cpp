374ca1e1695c1e9bee405045b29554569af032ff
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-09-16 15:37:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (77.02%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        while(n>1){
+            if(n%2==0){
+                n /=2;
+            }
+            else if(n%3==0){
+                n/=3;
+            }
+            else if(n%5==0){
+                n/=5;
+            }
+            else{
+                return false;
+            }
+        }
+        if(n==1){
+            return true;
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
