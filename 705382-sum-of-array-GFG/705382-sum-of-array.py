57a81c3568edc044c88d84b83054d28ab0a0f114
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-09-22 16:19:11 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 705382-sum-of-array-GFG/705382-sum-of-array.py
@@ -0,0 +1,11 @@
+class Solution:
+	def arraySum(self, arr):
+   		# code here
+   		sum = 0
+   	    for el in arr:
+   	        sum += el
+   	    return sum    
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
