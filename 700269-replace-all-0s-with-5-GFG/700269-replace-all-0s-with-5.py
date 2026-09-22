2796e0a9f6fa3df579c74ddb799147d5d0bdb463
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-09-22 18:12:25 +0530

    Added solution - Time: O(log n), Space: O(log n)

---
+++ 700269-replace-all-0s-with-5-GFG/700269-replace-all-0s-with-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertFive(self, n):
+        # code here
+        return int(str(n).replace('0','5'))       
+            
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
