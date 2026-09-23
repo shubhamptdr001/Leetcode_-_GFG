428773b92f3f19f2ae09795755617cd75b5222a8
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-09-23 17:38:27 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704983-sum-of-natural-numbers-GFG/704983-sum-of-natural-numbers.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+# code here
+print(n*(n+1)//2)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
