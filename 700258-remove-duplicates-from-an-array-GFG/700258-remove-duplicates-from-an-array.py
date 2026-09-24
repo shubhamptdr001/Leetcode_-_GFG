4774126fc241e3b68ef4f33d8c88146ba206caff
Shubham Patidar <sp26619atidarnimcet@gmail.com>
2026-09-24 12:10:34 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 700258-remove-duplicates-from-an-array-GFG/700258-remove-duplicates-from-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def remDuplicate(self, arr):
+        # code here 
+        return list(set(arr))
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
